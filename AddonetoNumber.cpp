2a5df0ee15777b2be79466255bc47640d2a724d5
vaibhavsethia <40268761+vaibhavsethia@users.noreply.github.com>
2018-08-16 17:49:14 +0530

    Create AddonetoNumber.cpp

---
+++ AddonetoNumber.cpp
@@ -0,0 +1,34 @@
+vector<int> Solution::plusOne(vector<int> &A) {
+    int i;
+    i = A.size() - 1;
+    if(A[i] < 9){     //for 522+1
+        A[i]++;
+    }
+    else{         //for 35459+1
+        A[i] = 0;
+        if(i == 0){     //for 09
+            A.insert(A.begin(), 1);
+        }
+        else{     //for 324689
+            A[i-1]++;
+        }
+        i--;
+        while(A[i] >= 10){    //make it one digit 
+            A[i] = 0;
+            if(i == 0){
+                A.insert(A.begin(), 1);
+                break;
+            }
+            else{
+                A[i-1]++;
+            }
+            i--;
+        }
+    }
+
+    while(*A.begin() == 0){
+        A.erase(A.begin());
+    }
+
+    return A;
+}
